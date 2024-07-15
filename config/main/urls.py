48ff326907d960c9074a71bc56fcabb64e800e6b
Idirisd <idirismazitov13@gmail.com>
2024-07-15 18:57:25 +0600

    .

--- config/main/urls.py
+++ config/main/urls.py
@@ -1,7 +1,6 @@
-from .views import RegisterView, ActivateAccountView, CustomLoginView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HouseViewSet, ApartmentViewSet, BookApartmentView, CancelBookingView
+from .views import RegisterView, ActivateAccountView, LoginView, HouseViewSet, ApartmentViewSet, BookApartmentView, CancelBookingView
 
 router = DefaultRouter()
 router.register(r'houses', HouseViewSet)
@@ -9,8 +8,9 @@ router.register(r'apartments', ApartmentViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('activate/', ActivateAccountView.as_view(), name='activate'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('activate-account/', ActivateAccountView.as_view(), name='activate-account'),
+    path('login/', LoginView.as_view(), name='login'),
     path('book-apartment/<int:pk>/', BookApartmentView.as_view(), name='book-apartment'),
     path('cancel-booking/<int:pk>/', CancelBookingView.as_view(), name='cancel-booking'),
+    path('', include(router.urls)),
 ]
--- config/main/views.py
+++ config/main/views.py
@@ -6,22 +6,15 @@ from django_filters import rest_framework as filters
 from .models import House, Apartment
 from .serializers import HouseSerializer, ApartmentSerializer
 
-
 from django.urls import reverse
 from django.contrib.auth.views import LoginView as AuthLoginView
 
 
-
-# 1
-# Представление для регистрации пользователей.
-# Использует CreateAPIView, которое предоставляет стандартное поведение для создания объектов
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-# 2
-# Представление для активации аккаунта.
-# Принимает POST-запрос, проверяет данные и активирует аккаунт.
+
 class ActivateAccountView(generics.GenericAPIView):
     serializer_class = ActivateAccountSerializer
 
@@ -32,9 +25,6 @@ class ActivateAccountView(generics.GenericAPIView):
         return Response({'message': 'Аккаунт успешно активирован'}, status=status.HTTP_200_OK)
 
 
-# 3
-# Представление для входа в систему.
-# Принимает POST-запрос с данными для входа, проверяет их и возвращает токены для аутентификации.
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
@@ -45,17 +35,13 @@ class LoginView(generics.GenericAPIView):
         tokens = serializer.create(user)
         return Response(tokens, status=status.HTTP_200_OK)
 
-# 4
-# ViewSet для работы с домами. Предоставляет стандартные CRUD операции.
-# Доступно для аутентифицированных пользователей или для чтения без аутентификации.
+
 class HouseViewSet(viewsets.ModelViewSet):
     queryset = House.objects.all()
     serializer_class = HouseSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-# 5
-# Фильтр для квартир, позволяет фильтровать по количеству комнат и этажу.
 class ApartmentFilter(filters.FilterSet):
     number_of_rooms = filters.NumberFilter(field_name="number_of_rooms", label="Количество комнат")
     floor = filters.NumberFilter(field_name="floor", label="пол")
@@ -65,11 +51,6 @@ class ApartmentFilter(filters.FilterSet):
         fields = ['number_of_rooms', 'floor']
 
 
-
-# 6
-# ViewSet для работы с квартирами. Предоставляет стандартные CRUD операции.
-# Доступно для аутентифицированных пользователей или для чтения без аутентификации.
-# Поддерживает фильтрацию по количеству комнат и этажу.
 class ApartmentViewSet(viewsets.ModelViewSet):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
@@ -77,9 +58,6 @@ class ApartmentViewSet(viewsets.ModelViewSet):
     filterset_class = ApartmentFilter
 
 
-# 7
-# Представление для бронирования квартиры. Проверяет,
-# забронирована ли квартира, и если нет, бронирует её для текущего пользователя.
 class BookApartmentView(generics.UpdateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
@@ -96,10 +74,6 @@ class BookApartmentView(generics.UpdateAPIView):
         return Response({"detail": "Квартира успешно забронирована."}, status=status.HTTP_200_OK)
 
 
-# 8
-# Представление для отмены бронирования квартиры.
-# Проверяет, забронирована ли квартира, и если да, снимает бронь.
-# Пользователь должен быть тем, кто забронировал, или администратором.
 class CancelBookingView(generics.UpdateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
@@ -120,9 +94,6 @@ class CancelBookingView(generics.UpdateAPIView):
         return Response({"detail": "Бронирование успешно отменено."}, status=status.HTTP_200_OK)
 
 
-
-
 class CustomLoginView(AuthLoginView):
     def get_success_url(self):
-        # Здесь можно указать URL, на который будет выполнено перенаправление после входа
         return reverse('book-apartment', kwargs={'pk': self.request.user.pk})
